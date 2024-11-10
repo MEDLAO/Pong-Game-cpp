48f6406464900cd2edfc5449335f57a41d66fb56
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-11-10 15:06:54 +0100

    Draw the paddles and the ball

--- Pong-Game-C++/main.cpp
+++ Pong-Game-C++/main.cpp
@@ -20,6 +20,11 @@ int main(int argc, const char * argv[]) {
     while (WindowShouldClose() == false) {
         BeginDrawing();
         
+        //Drawing
+        DrawCircle(screen_width / 2, screen_height / 2, 20, WHITE);
+        DrawRectangle(10, screen_height / 2 - 60, 25, 120, WHITE);
+        DrawRectangle(screen_width - 35, screen_height / 2 - 60, 25, 120, WHITE);
+        DrawLine(screen_width / 2, 0, screen_width / 2, screen_height, WHITE);
         EndDrawing();
     }
     
