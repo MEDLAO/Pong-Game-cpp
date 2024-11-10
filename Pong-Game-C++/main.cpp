6fd5c4525595ca5ad8e15247c78f7534f8912d0e
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-11-10 14:38:31 +0100

    implement blank screen + game loop

--- Pong-Game-C++/main.cpp
+++ Pong-Game-C++/main.cpp
@@ -12,5 +12,17 @@ using namespace std;
 
 int main(int argc, const char * argv[]) {
     cout << "Starting the game" << endl;
+    const int screen_width = 1280;
+    const int screen_height = 800;
+    InitWindow(screen_width, screen_height, "Pong Game C++");
+    SetTargetFPS(60);
+    
+    while (WindowShouldClose() == false) {
+        BeginDrawing();
+        
+        EndDrawing();
+    }
+    
+    CloseWindow();
     return 0;
 }
