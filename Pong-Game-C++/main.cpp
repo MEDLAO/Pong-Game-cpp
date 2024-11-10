6544eb63616b3b4030c5469ab3b8c6deecbda8e4
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-11-10 15:33:37 +0100

    Add movement logic for the ball

--- Pong-Game-C++/main.cpp
+++ Pong-Game-C++/main.cpp
@@ -10,6 +10,24 @@
 
 using namespace std;
 
+class Ball{
+public:
+    float x, y;
+    int speed_x, speed_y;
+    int radius;
+    
+    void draw(){
+        DrawCircle(x, y, radius, WHITE);
+    }
+    
+    void update(){
+        x += speed_x;
+        y += speed_y;
+    }
+};
+
+Ball ball;
+
 int main(int argc, const char * argv[]) {
     cout << "Starting the game" << endl;
     const int screen_width = 1280;
@@ -17,14 +35,26 @@ int main(int argc, const char * argv[]) {
     InitWindow(screen_width, screen_height, "Pong Game C++");
     SetTargetFPS(60);
     
+    ball.radius = 20;
+    ball.x = screen_width / 2;
+    ball.y = screen_height / 2;
+    ball.speed_x = 7;
+    ball.speed_y = 7;
+    
+    
     while (WindowShouldClose() == false) {
         BeginDrawing();
         
+        //Updating
+        ball.update();
+        
         //Drawing
-        DrawCircle(screen_width / 2, screen_height / 2, 20, WHITE);
+        ClearBackground(BLACK);
+        DrawLine(screen_width / 2, 0, screen_width / 2, screen_height, WHITE);
+        ball.draw();
         DrawRectangle(10, screen_height / 2 - 60, 25, 120, WHITE);
         DrawRectangle(screen_width - 35, screen_height / 2 - 60, 25, 120, WHITE);
-        DrawLine(screen_width / 2, 0, screen_width / 2, screen_height, WHITE);
+        
         EndDrawing();
     }
     
